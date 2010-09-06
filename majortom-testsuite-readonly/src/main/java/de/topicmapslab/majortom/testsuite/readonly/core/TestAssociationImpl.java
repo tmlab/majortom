4774ccfad35d8e58a06eb508411fff62a9d648d6
hass <hass@localhost>
2010-09-06 14:48:09 +0200

    create read only test suite and update parent pom

---
+++ majortom-testsuite-readonly/src/main/java/de/topicmapslab/majortom/testsuite/readonly/core/TestAssociationImpl.java
@@ -0,0 +1,75 @@
+package de.topicmapslab.majortom.testsuite.readonly.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestAssociationImpl {
+
+	@Test
+	public void testCreateRole() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParent() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRoleTypes() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRoles() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRolesTopic() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetType() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRolesIntInt() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRolesIntIntComparatorOfRole() {
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumberOfRoles() {
+
+		fail("Not yet implemented");
+	}
+}
