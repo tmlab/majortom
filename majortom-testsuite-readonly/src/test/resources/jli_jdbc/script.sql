5690457b12cccf1e051fc0531d08d34bcd94fab5
hass <hass@localhost>
2010-09-06 14:51:25 +0200

    remove obsolte files

--- majortom-testsuite-readonly/src/test/resources/jli_jdbc/script.sql
+++
@@ -1,8 +0,0 @@
-USE jli_test;
-
-DROP TABLE IF EXISTS testGetAssociationsPlayed;
-CREATE TABLE testGetAssociationsPlayed (id INTEGER UNIQUE NOT NULL, si VARCHAR(255) UNIQUE NOT NULL, id2 INTEGER);
-INSERT INTO testGetAssociationsPlayed VALUES(1,"http://TestTopicImpl/testGetAssociationsPlayed/1",NULL);
-INSERT INTO testGetAssociationsPlayed VALUES(2,"http://TestTopicImpl/testGetAssociationsPlayed/2",3);
-INSERT INTO testGetAssociationsPlayed VALUES(3,"http://TestTopicImpl/testGetAssociationsPlayed/3",NULL);
-
