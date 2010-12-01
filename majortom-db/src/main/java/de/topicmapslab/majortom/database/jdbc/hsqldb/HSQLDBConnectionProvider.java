69bc6b465a517c93e243d07081597f7865eb0716
jaehnichen <jaehnichen@localhost>
2010-12-01 17:03:35 +0100

    add hsqldb connection provider and session implementation

--- majortom-db/src/main/java/de/topicmapslab/majortom/database/jdbc/hsqldb/HSQLDBConnectionProvider.java
+++ majortom-db/src/main/java/de/topicmapslab/majortom/database/jdbc/hsqldb/HSQLDBConnectionProvider.java
@@ -3,11 +3,18 @@ package de.topicmapslab.majortom.database.jdbc.hsqldb;
 import de.topicmapslab.majortom.database.jdbc.rdbms.RDBMSConnectionProvider;
 import de.topicmapslab.majortom.database.jdbc.rdbms.RDBMSQueryProcessor;
 import de.topicmapslab.majortom.database.jdbc.rdbms.RDBMSSession;
+import de.topicmapslab.majortom.util.DatatypeAwareUtils;
 
 public class HSQLDBConnectionProvider  extends RDBMSConnectionProvider{
 	
 	public HSQLDBConnectionProvider() {
 		RDBMSQueryProcessor.GENERATED_KEY_COLUMN_NAME = "ID";
+	}
+	
+	public HSQLDBConnectionProvider(String host, String database, String user, String password) {
+		super(host, database, user, password);
+		RDBMSQueryProcessor.GENERATED_KEY_COLUMN_NAME = "ID";
+		DatatypeAwareUtils.setDateTimeFormat("yyyy-MM-dd HH:mm:ss.SSSSS+HH:mm");
 	}
 	
 	protected String getRdbmsName() {
@@ -23,7 +30,7 @@ public class HSQLDBConnectionProvider  extends RDBMSConnectionProvider{
 	 */
 	@SuppressWarnings("unchecked")
 	public RDBMSSession openSession() {		
-		return super.openSession();
+		return new HSQLDBSession(this, getUrl(), getUser(), getPassword());
 	}
 
 }
