bb15a1473f617f620d298bb3dde3da777a249e82
niederhausen <niederhausen@localhost>
2010-12-08 10:02:18 +0100

    last changes.. console app, i think

---
+++ majortom-postgresimporter/src/main/resources/dbimport.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [[ ! -d $JAVA_HOME ]] ;
+then
+  echo JAVA_HOME not defined or does not exist!
+else
+  SCRIPT_LOCATION=`dirname "$0"`
+  "${JAVA_HOME}/bin/java" $JAVA_OPTS -Djava.ext.dirs=$SCRIPT_LOCATION/libs -jar $SCRIPT_LOCATION/dbimporter-${project.version}.jar $*
+fi
