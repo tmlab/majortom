1bf5f565ab46da1afa78be9f6c558425c3b0f641
krosse <krosse@localhost>
2011-04-29 11:17:52 +0200

    add tests

---
+++ majortom-inmemory/src/test/java/TestBL.java
@@ -0,0 +1,56 @@
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.tmapi.core.Topic;
+import org.tmapi.core.TopicMapSystemFactory;
+import org.tmapix.io.XTMTopicMapReader;
+
+import de.topicmapslab.majortom.model.core.ITopic;
+import de.topicmapslab.majortom.model.core.ITopicMap;
+
+/*******************************************************************************
+ * Copyright 2010, Topic Map Lab ( http://www.topicmapslab.de )
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+/**
+ * @author Sven Krosse
+ *
+ */
+public class TestBL extends TestCase {
+
+	ITopicMap tm;
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	protected void setUp() throws Exception {
+		
+		tm = (	ITopicMap)TopicMapSystemFactory.newInstance().newTopicMapSystem().createTopicMap("http://example.org");
+		
+		XTMTopicMapReader reader = new XTMTopicMapReader(tm, new File("src/test/resources/toy.xtm"));
+		reader.read();
+		
+		
+	}
+	
+	
+	public void testname() throws Exception {
+		ITopic t = (ITopic)tm.getTopicBySubjectIdentifier(tm.createLocator("http://de.wikipedia.org/wiki/Deutschland"));
+		assertNotNull(t);
+		System.out.println(t.getBestLabel());
+	}
+	
+}
